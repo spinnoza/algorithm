5b468b04f7386b550d1a3a39e1e7b46661d6bc2a
Leo Wang <wangjil@delawareconsulting.com>
2023-06-29 15:36:08 +0800

    0629

---
+++ ConsoleApp1/DoubleNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class DoubleNode
+    {
+        public int Value { get; set; }
+
+        public DoubleNode Last { get; set; }
+        public DoubleNode Next { get; set; }
+
+        public DoubleNode(int data)
+        {
+            Value = data;
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,12 +10,25 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            for (int i = 0; i < 10; i++)
-            {
-                var arr = GenerateRandomArray(10, 100);
-                Console.WriteLine(ArrayToString(arr));
-                Console.WriteLine("================================");
-            }
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     var arr = GenerateRandomArray(10, 100);
+            //     Console.WriteLine(ArrayToString(arr));
+            //     Console.WriteLine("================================");
+            // }
+
+            var node = new Node(1);
+            var node2 = new Node(2);
+            var node3 = new Node(3);
+            var node4 = new Node(4);
+            node.Next = node2;
+            node2.Next = node3;
+            node3.Next = node4;
+            PrintLinkList(node);
+            var rNode  = ReverseLinkedList(node);
+            PrintLinkList(rNode);
+
+
             Console.ReadKey();
         }
 
@@ -57,6 +70,50 @@ namespace ConsoleApp1
             }
 
             return arr;
+        }
+
+        /// <summary>
+        /// 打印单项链表
+        /// </summary>
+        /// <param name="head"></param>
+        public static void PrintLinkList(Node head)
+        {
+            if (head==null)
+            {
+                return;
+            }
+
+            do
+            {
+                Console.WriteLine(head.Value);
+                head = head.Next;
+            } while (head!=null);
+        }
+
+        /// <summary>
+        /// 反转单项链表,返回新头
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static Node ReverseLinkedList(Node head)
+        {
+            if (head==null||head.Next==null)
+            {
+                return head;
+            }
+
+            Node pre = null;
+
+            while (head!=null)
+            {
+                var next = head.Next;
+                head.Next = pre;
+                pre = head;
+                head = next;
+            }
+
+            return pre;
+            
         }
     }
 }
