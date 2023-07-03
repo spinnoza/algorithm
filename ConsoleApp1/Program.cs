f160220b9d022ed5268d0d19b68f4ed5846711f2
Leo Wang <wangjil@delawareconsulting.com>
2023-07-03 13:26:40 +0800

    addMergeSort

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleApp1
@@ -16,20 +18,133 @@ namespace ConsoleApp1
             //     Console.WriteLine(ArrayToString(arr));
             //     Console.WriteLine("================================");
             // }
+            //
+            // var node = new Node(1);
+            // var node2 = new Node(2);
+            // var node3 = new Node(3);
+            // var node4 = new Node(4);
+            // node.Next = node2;
+            // node2.Next = node3;
+            // node3.Next = node4;
+            // PrintLinkList(node);
+            // var rNode  = ReverseLinkedList(node);
+            // PrintLinkList(rNode);
 
-            var node = new Node(1);
-            var node2 = new Node(2);
-            var node3 = new Node(3);
-            var node4 = new Node(4);
-            node.Next = node2;
-            node2.Next = node3;
-            node3.Next = node4;
-            PrintLinkList(node);
-            var rNode  = ReverseLinkedList(node);
-            PrintLinkList(rNode);
+
+            int testTime = 500000;
+            int maxSize = 100;
+            int maxValue = 100;
+            bool succeed = true;
+            for (int i = 0; i < testTime; i++)
+            {
+                int[] arr1 = GenerateRandomArray(maxSize, maxValue);
+                int[] arr2 = CopyArray(arr1);
+                MergeSort1(arr1);
+                Array.Sort(arr2);
+                if (!IsEqual(arr1, arr2))
+                {
+                    succeed = false;
+                    ArrayToString(arr1);
+                    ArrayToString(arr2);
+                    break;
+                }
+            }
+            Console.WriteLine(succeed ? "Nice!" : "Oops!");
 
 
             Console.ReadKey();
+        }
+
+        public static void MergeSort1(int[] arr)
+        {
+            if (arr == null || arr.Length < 2)
+            {
+                return;
+            }
+            Process(arr, 0, arr.Length - 1);
+        }
+
+        public static void Process(int[] arr, int L, int R)
+        {
+            if (L<R)
+            {
+                var M = L+ ((R-L)>>1);
+                Process(arr, L, M);
+                Process(arr, M + 1, R);
+                Merge(arr, L, M, R);
+            }
+        }
+
+        public static void Merge(int[] arr, int L, int M, int R)
+        {
+            var help = new int[R-L+1];
+            var i = 0;
+            var LIndex = L;
+            var RIndex = M+1;
+
+            while (LIndex<=M&&RIndex<=R)
+            {
+                help[i++] = arr[LIndex] <= arr[RIndex]? arr[LIndex++] : arr[RIndex++];
+            }
+
+            if (LIndex<=M)
+            {
+                while (LIndex<=M)
+                {
+                    help[i++] = arr[LIndex++];
+                }
+            }
+
+            if (RIndex<=R)
+            {
+                while (RIndex<=R)
+                {
+                    help[i++] = arr[RIndex++];
+                }
+            }
+
+            // var k = 0;
+            // for (int j = L; j < R; j++)
+            // {
+            //     arr[j]= help[k++]; 
+            // }
+
+            for (int j = 0; j < help.Length; j++)
+            {
+                arr[L+j] = help[j];
+            }
+
+
+        }
+
+        /// <summary>
+        /// 比较2个数组是否相等
+        /// </summary>
+        /// <param name="arr1"></param>
+        /// <param name="arr2"></param>
+        /// <returns></returns>
+        public static bool IsEqual(int[] arr1, int[] arr2)
+        {
+            if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null))
+            {
+                return false;
+            }
+            if (arr1 == null && arr2 == null)
+            {
+                return true;
+            }
+            if (arr1.Length != arr2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         /// <summary>
@@ -70,6 +185,24 @@ namespace ConsoleApp1
             }
 
             return arr;
+        }
+        /// <summary>
+        /// copy 数组
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int[] CopyArray(int[] arr)
+        {
+            if (arr == null)
+            {
+                return null;
+            }
+            int[] res = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res[i] = arr[i];
+            }
+            return res;
         }
 
         /// <summary>
